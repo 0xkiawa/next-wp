3cff3bae17c6266361e1f15795dc401d0a09a8f9
kiawa <kiawavictor@gmail.com>
2026-03-24 17:44:33 +0300

    ba

--- app/mainsection/the-unsubscribed.tsx
+++ app/mainsection/the-unsubscribed.tsx
@@ -37,7 +37,7 @@ export default async function Page({ excludedPostIds = [] }: UnsubscribedProps) 
           Echoes of the past, resonating in the present. A curated collection of timeless stories.
         </p>
         <p className="text-base md:text-lg font-acaslon italic text-black">
-          with <span className="font-space-mono font-bold not-italic text-black ml-[2px]">KIAWA VURNER</span>
+          with <span className="font-space-mono font-bold not-italic text-black ml-[2px]">KIAWAVURNER</span>
         </p>
       </div>
 
