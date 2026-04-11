b07f232fac195dbf073145258d40b74a697e4310
kiawa <kiawavictor@gmail.com>
2026-04-11 15:16:14 +0300

    b

--- app/page.tsx
+++ app/page.tsx
@@ -126,7 +126,7 @@ export default async function Home() {
             alt="KiawaNotes"
             width={48}
             height={48}
-            className="flex-shrink-0 object-cover rounded-1xl"
+            className="flex-shrink-0 object-cover rounded-[10px]"
           />
           <p className="font-acaslon text-sm text-black leading-snug">
             Support{" "}
